3c1ae2887fc56bdc30c6cf82d83efcdf3f0e2f32
carlos_diaz_07 <diazmelian.carlos@gmail.com>
2021-07-31 17:03:25 +0000

    "coming soon" message create & files re-organize

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './img/Logo.svg';
+import './css/App.css';
 
 function App() {
   return (
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Carlos Diaz portfolio Coming soon
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
